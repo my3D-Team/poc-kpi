c8b80aa11cf6dd3191ad747e46b0319cdc692a9a
DAMIE, Nathan <ndamie@LFR410479.corp.capgemini.com>
2015-10-13 12:30:58 +0200

    [init] Create new app structure for kpi impl

---
+++ app/js/components/myApp.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ndamie on 13/10/2015.
+ */
+
+"use strict";
+
+var React = require('react');
+
+var App = React.createClass({
+
+    render: function () {
+        return (
+            <div>
+                <h1>Hello Al</h1>
+                <img src="http://cimg.tvgcdn.net/i/r/2012/08/09/a45fb16a-e10c-452d-8eb4-e7fe001df6e8/crop/660x494+0+0/thumbnail/350x262/c27ced3499ab418ef64ab9dbc8481368/120809alf1.jpg"></img>
+            </div>
+            );
+    }
+});
+
+module.exports = App;
+
---
+++ app/js/main.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var App = require('./components/myApp');
+
+React.render(React.createElement(App), document.getElementById('content'));
